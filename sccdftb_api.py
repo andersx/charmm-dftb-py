5582f42a98ff34df3febdc54b79aedf071b51a29
Anders S. Christensen <andersx@nano.ku.dk>
2014-11-05 14:33:36 -0600

    Added initial option parser

---
+++ sccdftb_api.py
@@ -0,0 +1,44 @@
+from argparse import ArgumentParser
+from datetime import datetime
+from uuid import uuid4
+from sccdftb_config import SCRATCH_DIR, CHARMM_EXE
+
+def get_time_string():
+    d = datetime.now()
+    return str(d).replace(" ","_")
+
+def generate_scr_name():
+    scr = SCRATCH_DIR + "/" + get_time_string() + "_" + str(uuid4())
+    return scr
+
+
+def get_args():
+    parser = ArgumentParser(
+        description="Run a DFTB3 calculation through CHARMM.")
+
+    parser.add_argument("--ixyz", "-i", action='store', const=None,
+        help="Input .xyz file.")
+
+    parser.add_argument("--oxyz", "-o", action='store', const=None,
+        help="Output .xyz file (for geometry optimization).")
+
+    parser.add_argument('--d2', '-d2', action='store_const', const=2)
+    parser.add_argument('--d3', '-d3', action='store_const', const=3)
+    parser.add_argument('--cpe', '-cpe', action='store_const', const=3)
+
+    parser.add_argument("--minimize", "-m", action='store_true',
+        help="Perform geometry optimization.")
+
+    parser.add_argument('--scf-tol', "-s" , action='store_const', const=1.0e-7,
+        help="SCF energy convergence criterion (default:  1e-7 Hartree).")
+
+    parser.add_argument('--charge', "-c" , action='store_const', const=0,
+        help="Total charge of the system (default:  0 a.u.).")
+
+
+    args = parser.parse_args()
+
+    return args
+
+
+
---
+++ sccdftb_config.py
@@ -0,0 +1,5 @@
+# User scratch directory
+SCRATCH_DIR = "/home/andersx/scr"
+
+# Executable for CHARMM
+CHARMM_EXE = "/home/andersx/dev/charmm_cpe/exec/gnu/charmm"
